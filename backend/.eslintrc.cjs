c40020a70f1a0e591f52c8c299779ee219aa9329
CarlosRufianDev <carlosrufiandev@gmail.com>
2025-08-10 21:45:58 +0200

    chore: relax eslint rules and fix pre-commit

--- backend/.eslintrc.cjs
+++ backend/.eslintrc.cjs
@@ -4,9 +4,15 @@ module.exports = {
   extends: ['standard'],
   parserOptions: { ecmaVersion: 'latest' },
   rules: {
-    'no-console': 'off',
-    'promise/param-names': 'off',
-    'n/no-unsupported-features/es-builtins': 'off'
+    // Permitimos ; para no hacer refactor masivo ahora
+    semi: 'off',
+    // Evitar miles de diffs ahora
+    'space-before-function-paren': 'off',
+    'no-multiple-empty-lines': ['warn', { max: 3 }],
+    'no-trailing-spaces': 'warn',
+    'object-shorthand': 'off',
+    // Mantén logs (los revisarás luego)
+    'no-console': 'off'
   },
   ignorePatterns: ['node_modules/', 'coverage/']
-}
+};
